b8c790226b0cf2c56796e2f547f397430e28e0d5
김선빈 <ksb9242@gmail.com>
2021-09-07 17:12:43 +0900

    git init & 도메인 분석 설계

---
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -0,0 +1,27 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+import static javax.persistence.FetchType.LAZY;
+
+@Entity
+@Getter
+@Setter
+public class Delivery {
+    @Id
+    @GeneratedValue
+    @Column(name = "delivery_id")
+    private Long id;
+
+    @OneToOne(mappedBy = "delivery", fetch = LAZY)
+    private Order order;
+
+    @Embedded
+    private Address address;
+
+    @Enumerated(EnumType.STRING)    // ORDINAL 사용 시 1,2 로 들어가는데 status 추가 시 장애 날 확률 높음
+    private DeliveryStatus status;  //READY, COMP
+}
