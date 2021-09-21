d4828377a82154d3541c87af4e85104b8181cfeb
김선빈 <ksb9242@gmail.com>
2021-09-21 21:58:47 +0900

    jpashop

--- src/main/java/jpabook/jpashop/controller/TestController.java
+++ src/main/java/jpabook/jpashop/controller/TestController.java
@@ -7,14 +7,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
-//    @Autowired
-//    private TimeMapper timeMapper;
-//
-//    @RequestMapping("/test")
-//    public String test() {
-//        System.out.println("timeMapper.getTime();" + timeMapper.getTime());
-//
-//        return "OK";
-//    }
+    @Autowired
+    private TimeMapper timeMapper;
+
+    @RequestMapping("/test")
+    public String test() {
+        System.out.println("timeMapper.getTime();" + timeMapper.getTime());
+
+        return "OK";
+    }
 
 }
